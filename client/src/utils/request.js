24292f89d82feed5be0f96a2b7821bf2b1a2b4b7
Nader Gh <nader.ghanawi78@gmail.com>
2025-08-27 20:41:56 +0300

    first commit

---
+++ client/src/utils/request.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+/*===========================================*/
+/*===========================================*/
+/*===========================================*/
+
+// const localy = "http://localhost:3001";
+
+// const production = "https://online-courses-mern-stack.onrender.com";
+
+const request = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:3001",
+  withCredentials: true
+});
+
+export default request; 
